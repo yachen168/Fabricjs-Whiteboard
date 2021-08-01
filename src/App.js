99507dc70e2cbb775521355d5e6e45dd2199be29
yachen <io260188@gmail.com>
2021-08-01 21:52:19 +0800

    feat: toJson & fromJson

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ let currentColor = "#000000";
 let currentWidth = 1;
 let group = {};
 
+const modes = {
+  pan: "pan",
+  drawing: "drawing",
+};
+
 const initCanvas = () =>
   new fabric.Canvas("canvas", {
     height: 500,
@@ -82,15 +87,18 @@ function createTriangle(canvas) {
 const addCanvasEventListeners = (canvas) => {
   canvas.on("mouse:down", (event) => {
     isMousePressed = true;
-
-    if (currentMode === "pencil") {
-      canvas.setCursor("crosshair");
-      canvas.requestRenderAll();
-    }
   });
 
   canvas.on("mouse:move", (event) => {
-    if (isMousePressed && currentMode === "pencil") {
+    if (currentMode === modes.drawing) {
+      canvas.setCursor("crosshair");
+      canvas.requestRenderAll();
+    }
+
+    if (isMousePressed && currentMode === modes.pan) {
+      canvas.setCursor("grab");
+      canvas.requestRenderAll();
+    } else if (isMousePressed && currentMode === modes.drawing) {
       canvas.isDrawingMode = true;
       canvas.freeDrawingBrush.color = currentColor;
       canvas.freeDrawingBrush.width = currentWidth;
@@ -101,13 +109,17 @@ const addCanvasEventListeners = (canvas) => {
 
   canvas.on("mouse:up", (event) => {
     isMousePressed = false;
-    canvas.setCursor("default");
-    canvas.requestRenderAll();
+    // canvas.setCursor("default");
+    // canvas.requestRenderAll();
   });
 };
 
 const toggleMode = (mode) => {
-  currentMode = mode;
+  if (currentMode === modes[mode]) {
+    currentMode = "";
+  } else {
+    currentMode = modes[mode];
+  }
 };
 
 const clearCanvas = (canvas) => {
@@ -116,6 +128,18 @@ const clearCanvas = (canvas) => {
       canvas.remove(item);
     }
   });
+};
+
+const canvasFromJson = (canvas) => {
+  const data = JSON.parse(
+    '{"version":"4.3.1","objects":[{"type":"circle","version":"4.3.1","originX":"left","originY":"top","left":100,"top":100,"width":40,"height":40,"fill":"rgba(255, 255, 255, 0.0)","stroke":"#000000","strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"radius":20,"startAngle":0,"endAngle":6.283185307179586},{"type":"text","version":"4.3.1","originX":"left","originY":"top","left":253,"top":102,"width":95.55,"height":18.08,"fill":"#000000","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":2.43,"scaleY":2.43,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"text":"Some text","fontSize":16,"fontWeight":"normal","fontFamily":"Arial","fontStyle":"normal","lineHeight":1.16,"underline":false,"overline":false,"linethrough":false,"textAlign":"left","textBackgroundColor":"","charSpacing":0,"minWidth":20,"splitByGrapheme":false,"styles":{}},{"type":"rect","version":"4.3.1","originX":"left","originY":"top","left":109,"top":212,"width":40,"height":40,"fill":"rgba(255, 255, 255, 0.0)","stroke":"#000000","strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"rx":0,"ry":0},{"type":"image","version":"4.3.1","originX":"left","originY":"top","left":499,"top":86,"width":1200,"height":900,"fill":"rgb(0,0,0)","stroke":null,"strokeWidth":0,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":0.2,"scaleY":0.2,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"cropX":0,"cropY":0,"src":"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg","crossOrigin":null,"filters":[]},{"type":"polyline","version":"4.3.1","originX":"left","originY":"top","left":483.78,"top":339.22,"width":50.88,"height":50.88,"fill":"white","stroke":"black","strokeWidth":2,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":262.52,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"points":[{"x":100,"y":100},{"x":99.11611652351681,"y":100.88388347648318},{"x":145.58058261758407,"y":147.34834957055045},{"x":142.92893218813452,"y":150},{"x":150,"y":150},{"x":150,"y":142.92893218813452},{"x":147.34834957055045,"y":145.58058261758407},{"x":100.88388347648318,"y":99.11611652351681},{"x":100,"y":100}]},{"type":"image","version":"4.3.1","originX":"left","originY":"top","left":197,"top":132,"width":1200,"height":900,"fill":"rgb(0,0,0)","stroke":null,"strokeWidth":0,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeUniform":false,"strokeMiterLimit":4,"scaleX":0.2,"scaleY":0.2,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"cropX":0,"cropY":0,"src":"https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg","crossOrigin":null,"filters":[]}]}'
+  );
+  canvas.loadFromJSON(data);
+  canvas.requestRenderAll();
+};
+
+const canvasToJson = (canvas) => {
+  console.log(JSON.stringify(canvas.toJSON()));
 };
 
 const groupObjects = (canvas, group, shouldGroup) => {
@@ -176,7 +200,7 @@ const App = () => {
       >
         image
       </button>
-      <button onClick={() => toggleMode("pencil")}>pencil</button>
+      <button onClick={() => toggleMode(modes.drawing)}>pencil</button>
       <input type="color" onChange={(e) => (currentColor = e.target.value)} />
       <input
         type="range"
@@ -191,6 +215,9 @@ const App = () => {
       <button onClick={() => groupObjects(canvas, group, false)}>
         ungroup
       </button>
+
+      <button onClick={() => canvasToJson(canvas)}>toJson</button>
+      <button onClick={() => canvasFromJson(canvas)}>fromJson</button>
       <input type="file" accept="image/*" onChange={uploadImage(canvas)} />
       <canvas id="canvas"></canvas>
     </div>
