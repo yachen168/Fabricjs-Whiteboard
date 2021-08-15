4bc6288c9818e4c36c64da2c56e0ee52d1ec8dc7
yachen <io260188@gmail.com>
2021-08-15 15:01:03 +0800

    refactor: constants

---
+++ src/components/WhiteBoard/constants.js
@@ -0,0 +1,40 @@
+export const drawingMode = {
+  RECTANGLE: 'RECTANGLE',
+  ELLIPSE: 'ELLIPSE',
+  TRIANGLE: 'TRIANGLE',
+  LINE: 'LINE',
+  ERASER: 'ERASER',
+};
+
+export const defaultRectOptions = {
+  strokeWidth: 2,
+  stroke: 'black',
+  fill: 'transparent',
+  strokeUniform: true,
+  noScaleCache: false,
+  objectCaching: false,
+};
+
+export const defaultEllipseOptions = {
+  strokeWidth: 2,
+  stroke: 'black',
+  fill: 'black',
+  strokeUniform: true,
+  noScaleCache: false,
+};
+
+export const defaultTriangleOptions = {
+  strokeWidth: 2,
+  stroke: 'black',
+  fill: 'black',
+  strokeUniform: true,
+  noScaleCache: false,
+};
+
+export const defaultLineOptions = {
+  strokeWidth: 2,
+  stroke: 'black',
+  fill: 'black',
+  strokeUniform: true,
+  noScaleCache: false,
+};
