bef25cc5b7eb1e5b046ab6cd806fe09a162fd5e8
yachen <io260188@gmail.com>
2021-08-15 15:44:46 +0800

    refactor: plugins folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './plugins';
 import App from './App';
 
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/plugins/primereact.js
@@ -0,0 +1,3 @@
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
